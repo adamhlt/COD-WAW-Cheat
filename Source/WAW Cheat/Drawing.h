49ebbc21a2abfc29d33392a453ff54b52006f442
Adam <henault.adam@gmail.com>
2021-11-22 00:24:13 +0100

    Fix Drawing

--- Source/WAW Cheat/Drawing.h
+++ Source/WAW Cheat/Drawing.h
@@ -9,13 +9,12 @@ public:
 	static bool bExit; // boolean to unhook the dll
 	static bool bDisplay; // boolean to display the cheat menu
 	static bool bDisplayOverlay; // boolean to display the cheat overlay
+	static BOOL bInit; // boolean to initialize imgui
 
 	static HRESULT APIENTRY hkEndScene(LPDIRECT3DDEVICE9 D3D9Device);
 	static void DrawLine(int x, int y, int x1, int y1, int thickness, D3DCOLOR color); // 
 
 private:
-	static BOOL bInit; // boolean to initialize imgui
-
 	static void InitImGui(LPDIRECT3DDEVICE9 D3D9Device);
 	static void DisplayImGui();
 	static void DisplayImGuiMenu();
